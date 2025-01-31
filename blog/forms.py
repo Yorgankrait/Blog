950d8261397513de68bef512c431345cbcb0caa5
Sergey Zhukov <yorgankrait@gmail.com>
2025-01-31 11:08:04 +0300

    add clock

--- blog/forms.py
+++ blog/forms.py
@@ -5,8 +5,24 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'category']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-        } 
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Добавляем классы Bootstrap и плейсхолдеры к полям
+        self.fields['title'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Введите заголовок поста'
+        })
+        self.fields['content'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Введите содержание поста',
+            'rows': '6'
+        })
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-control w-100'
+        })
+
+        # Изменяем метки полей
+        self.fields['title'].label = 'Заголовок'
+        self.fields['content'].label = 'Содержание'
+        self.fields['category'].label = 'Категория' 
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -11,6 +11,38 @@ class UserRegisterForm(UserCreationForm):
         
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Добавляем классы Bootstrap к полям
-        for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control' 
+        # Добавляем классы Bootstrap и плейсхолдеры к полям
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Введите имя пользователя'
+        })
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Введите email'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Введите пароль'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Подтвердите пароль'
+        })
+
+        # Изменяем метки полей
+        self.fields['username'].label = 'Имя пользователя'
+        self.fields['email'].label = 'Email'
+        self.fields['password1'].label = 'Пароль'
+        self.fields['password2'].label = 'Подтверждение пароля'
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Введите имя пользователя'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control w-100',
+            'placeholder': 'Введите пароль'
+        }) 
